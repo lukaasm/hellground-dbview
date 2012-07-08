54ff5e2109edbfffb9ce6751c79cde9470765c69
lukaasm <lukaasm@gmail.com>
2012-07-08 13:03:33 +0200

    add CMake project

---
+++ src/LoginWidget.h
@@ -0,0 +1,30 @@
+/*
+*    Copyright (C) 2012 lukaasm <lukaasm.pl>
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Affero General Public License version 3 as
+*    published by the Free Software Foundation.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef H_LoginWidget
+#define H_LoginWidget
+
+#include <Wt/Ext/Dialog>
+
+class LoginWidget : public Wt::Ext::Dialog
+{
+    public:
+        LoginWidget(const Wt::WString&);
+
+
+};
+
+#endif
